064f4424c78cd0c1eedbfe4cc52e1dc6cf05db52
v.budarin <v.budarin@tinkoff.ru>
2017-03-15 09:43:53 +0300

    init commit

--- components/App.jsx
+++ components/App.jsx
@@ -5,9 +5,27 @@ import styles from './App.css';
 import Header from './Header.jsx';
 
 class App extends React.Component {
+    componentWillMount() {
+        this.setState({
+            showHeader: true
+        })
+    }
+
+    componentDidMount() {
+        setTimeout(()=> {
+            styles.unuse();
+        }, 5000);
+
+        setTimeout(()=> {
+            this.setState({
+                showHeader: false
+            })
+        }, 10000);
+    }
+
     render() {
         return (<div>
-            <Header />
+            {this.state.showHeader && <Header />}
             <div className={this.props.classeNames.app} >
                 Application text
             </div>
